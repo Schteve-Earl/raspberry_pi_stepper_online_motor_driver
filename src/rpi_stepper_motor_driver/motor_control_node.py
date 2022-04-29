99d1defeed32dcaba7522dd72051889d8351f51e
Stephen Earl <searl@bastiansolutions.com>
2022-04-29 12:35:12 -0600

    bug fixes

--- src/rpi_stepper_motor_driver/motor_control_node.py
+++ src/rpi_stepper_motor_driver/motor_control_node.py
@@ -2,26 +2,30 @@
 
 import rospy
 import actionlib
-from rpi_stepper_motor_driver.msg import ControlMotorAction
+import rpi_stepper_motor_driver.msg
 # import RPi.GPIO as GPIO
 
 
 class MotorControl(object):
     def __init__(self):
-        self.motor_as = actionlib.SimpleActionServer("/stepper_motor_control", ControlMotorAction, self.callback)
-        self.pulses_per_rotation_ = rospy.get_param("/motor_parameters/pulses_per_rotation")
+        self.motor_as = actionlib.SimpleActionServer("stepper_motor_control", rpi_stepper_motor_driver.msg.ControlMotorAction, self.callback)
+        self.pulses_per_rotation_ = rospy.get_param("/rpi_stepper_motor_driver/motor_parameters/pulses_per_rotation")
 
     def callback(self, goal):
-        result = action.ControlMotorResult
-        result.result = self.execute_move()
+        self.set_speed(goal.speed)
+        result = rpi_stepper_motor_driver.msg.ControlMotorResult()
+        # result.result = self.execute_move()
         self.motor_as.set_succeeded(result)
+
 
     def set_direction(self, direction: int):
         self.direction_ = direction
 
     def set_speed(self, speed: float):
         if speed < 0.01:
+            rospy.logwarn_once("Speed is too slow, setting to 0.1 rad/s")
             speed = 0.01
+        rospy.loginfo("speed is: %d", speed)
         self.speed_ = self.convert_speed_to_ms(speed)
 
     def set_rotation(self, rotation: float):
